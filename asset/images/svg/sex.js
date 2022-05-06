188012802042bd9e423aa2575dafd2278bf47c94
ieongch0106 <ieong.chou.heng@gmail.com>
2022-05-06 01:37:04 -0400

    questionnaire after signup.html, added effects and animations

---
+++ asset/images/svg/sex.js
@@ -0,0 +1,9 @@
+const male_icon = `<svg xmlns="http://www.w3.org/2000/svg" id='male_icon' width="125" height="125" fill="currentColor" class="bi bi-gender-male mt-5 col-lg-6" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2H9.5zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8z"/>
+</svg>`
+
+const female_icon = `<svg xmlns="http://www.w3.org/2000/svg" id='female_icon' width="125" height="125" fill="currentColor" class="bi bi-gender-female mt-5 col-lg-6" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5z"/>
+</svg>`
+
+export { male_icon, female_icon }
--- signup.js
+++ signup.js
@@ -1,3 +1,5 @@
+import { male_icon, female_icon } from "./asset/images/svg/sex.js";
+
 //for signup.html
 const background = 'url(asset/images/background.jpg)';
 const opacity = 'linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5))';
@@ -7,27 +9,41 @@ const pw = document.getElementById('sign_up_pw');
 const confirm_pw = document.getElementById('sign_up_confirm_pw');
 const join_us_button = document.getElementById('join_us')
 
-join_us_button.addEventListener('click', join_us);
+join_us_button.addEventListener('click', verify);
+
+function verify() {
+    join_us();
+}
 
 function join_us() {
     verify = true;
     if (verify) {
-        modify_background();
-        questionnaire();
+        const elements = document.getElementsByClassName('sign_up_section');
+        for (const e of elements) {
+            e.innerHTML = '';   
+        }
+        document.getElementById('questionnaire').classList.add('fade-in-down');
+        document.getElementById('box_card').innerHTML += '<h1>What is your sex?<h1><br>' + male_icon + female_icon;
+        document.getElementById('male_icon').addEventListener('click', choose_age);
+        document.getElementById('female_icon').addEventListener('click', choose_age);
     } else {
-        // alert()
+        // alert();
     }
 }
 
-function modify_background() {
-    document.body.style.visibility = 'hidden';
-    document.body.style.backgroundImage = `${opacity}, ${background}`;
-    document.body.style.backgroundSize = 'cover';
+function choose_age() {
+    restart_animation('questionnaire', 'fade-in-down');
+    const input = document.createElement('input');
+    input.type = 'range';
+    input.min = '15';
+    input.max = '80';
+    input.classList.add('col-lg-12', 'col-md-10', 'col-sm-10')
+    document.getElementById('box_card').innerHTML = '<h1>What is your age:<h1><br>';
+    document.getElementById('box_card').append(input);
 }
 
-function questionnaire() {
-    const box = document.createElement('div');
-    box.classList.add('card', 'bg-dark');
-    document.body.append(box);
-    console.log(document.body)
+function restart_animation(id_container, class_animation) {
+    document.getElementById(id_container).classList.remove(class_animation);
+    document.getElementById(id_container).offsetWidth;
+    document.getElementById(id_container).classList.add(class_animation);
 }
